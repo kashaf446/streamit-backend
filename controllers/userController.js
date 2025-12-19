1b6e451cdf32ca64fdd9afd067199ac2eb6c6f6c
kashaf446 <kashafurrahman446@gmail.com>
2025-12-20 02:00:49 +0530

    Refactor userController by removing registerUser
    Removed unused imports and cleaned up registerUser function.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,6 @@
-import User from "../models/User.js";
-import jwt from "jsonwebtoken";
-
-export const registerUser = async (req, res) => {
-  const user = await User.create(req.body);
-  res.json(user);
-};
-
 export const loginUser = async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
+
   if (!user) {
     return res.status(404).json({ message: "User not found" });
   }
