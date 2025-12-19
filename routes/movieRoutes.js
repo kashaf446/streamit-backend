a5dfdff0e8848f1a6f2f6054ed07ae2bced79a95
kashaf446 <kashafurrahman446@gmail.com>
2025-12-20 01:17:14 +0530

    Add authentication middleware to movie routes

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { addMovie, getMovies } from "../controllers/movieController.js";
 import auth from "../middleware/auth.js";
+
 const router = express.Router();
+
 router.post("/", auth, addMovie);
 router.get("/", getMovies);
+
 export default router;
