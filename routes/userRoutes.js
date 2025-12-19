62883e14b017cc6993136952720eb91f6946525b
kashaf446 <kashafurrahman446@gmail.com>
2025-12-20 01:18:55 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,9 @@
 import express from "express";
 import userController from "../controllers/userController.js";
+
 const router = express.Router();
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+
+router.post("/register", userController.registerUser);
+router.post("/login", userController.loginUser);
+
 export default router;
