cf005debcb0e29bd3202bd69d9dfd5e873318859
kashaf446 <kashafurrahman446@gmail.com>
2025-12-20 01:48:26 +0530

    Refactor user routes to use named imports

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import userController from "../controllers/userController.js";
+import { registerUser, loginUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post("/register", userController.registerUser);
-router.post("/login", userController.loginUser);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
